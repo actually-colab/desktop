f45d43b6ddc13e74d5532b780bbdeccc102d3ba4
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-05 14:14:53 -0500

    Added spacing to theme

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,7 +1,7 @@
 export const palette = {
   BASE: 'rgb(15, 19, 26)',
   BASE_FADED: '#1a1d24',
-  BASE_BORDER: '#444952',
+  BASE_BORDER: '#3C3F42',
   CHARCOAL: '#383F51',
   GRAY: '#8C8A93',
   OLD_LAVENDER: '#896978',
@@ -10,4 +10,14 @@ export const palette = {
   ERROR: '#F52F57',
   WARNING: '#FFBC42',
   SUCCESS: '#62C370',
+};
+
+export const spacing = {
+  DEFAULT: 16,
+  pad: (fallback: number, padding: { left?: number; right?: number; top?: number; bottom?: number }) => ({
+    paddingLeft: padding.left ?? fallback,
+    paddingRight: padding.right ?? fallback,
+    paddingTop: padding.top ?? fallback,
+    paddingBottom: padding.bottom ?? fallback,
+  }),
 };
