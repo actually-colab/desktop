ee07d53c5dc0d5af9c612a20e015fd21815bde62
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-06 15:56:32 -0500

    Build an Ace importer component

---
+++ src/utils/AceImports.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/theme-xcode';
+import 'ace-builds/src-noconflict/ext-language_tools';
+
+const AceImports = <React.Fragment />;
+
+export default AceImports;
