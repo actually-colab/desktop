faabe42442aad36f92aff22c6298fb87ce24e0f1
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-14 17:21:46 -0500

    Added auth redirect page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
 import { ipcRenderer } from 'electron';
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, useHistory, useLocation } from 'react-router-dom';
 import { StyleSheet, css } from 'aphrodite';
 
 import './App.global.less';
-import store from './redux';
-import { EditorPage } from './pages';
+import store, { ReduxState } from './redux';
+import { _auth } from './redux/actions';
 import { extractLoginData } from './utils/redirect';
+import { AuthPage, EditorPage } from './pages';
 
 ipcRenderer.on('login-success', (_, data: { url: string }) => {
   const loginResponse = extractLoginData(data.url);
 
   console.log(loginResponse);
 });
+
+const EntryPoint: React.FC = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const isSessionLoaded = useSelector((state: ReduxState) => state.auth.isSessionLoaded);
+  const isAuthenticated = useSelector((state: ReduxState) => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+  const dispatchLoadSession = React.useCallback(() => dispatch(_auth.loadSession()), [dispatch]);
+
+  React.useEffect(() => {
+    if (!isSessionLoaded) {
+      dispatchLoadSession();
+    }
+  }, [dispatchLoadSession, isSessionLoaded]);
+
+  React.useEffect(() => {
+    if (isSessionLoaded && !isAuthenticated && location.pathname !== '/auth') {
+      history.push('/auth');
+    } else if (isAuthenticated && location.pathname === '/auth') {
+      history.push('/');
+    }
+  }, [history, isAuthenticated, isSessionLoaded, location.pathname]);
+
+  return (
+    <Switch>
+      <Route path="/auth" component={AuthPage} />
+      <Route path="/" component={EditorPage} />
+    </Switch>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -28,9 +61,7 @@ export default function App() {
     <div className={css(styles.container)}>
       <Provider store={store}>
         <Router>
-          <Switch>
-            <Route path="/" component={EditorPage} />
-          </Switch>
+          <EntryPoint />
         </Router>
       </Provider>
     </div>
--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { StyleSheet, css } from 'aphrodite';
+import { Button } from 'rsuite';
+
+import { spacing } from '../../constants/theme';
+import { _auth } from '../../redux/actions';
+import { Header } from '../../components';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    overflow: 'hidden',
+  },
+  content: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signInContainerBackground: {
+    width: 512,
+    background: '-webkit-linear-gradient(top left, #f55673, #E2CC52)',
+    borderRadius: 20,
+  },
+  signInContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 4,
+    padding: 40,
+    borderRadius: 16,
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    background: '-webkit-linear-gradient(top left, #f55673, #E2CC52)',
+    '-webkit-background-clip': 'text',
+    '-webkit-text-fill-color': 'transparent',
+    '-webkit-user-select': 'none',
+  },
+  subtitle: {
+    fontSize: 18,
+  },
+  signInButton: {
+    marginTop: spacing.DEFAULT,
+  },
+});
 
 const AuthPage: React.FC = () => {
-  return <React.Fragment />;
+  const dispatch = useDispatch();
+
+  const dispatchOpenAuthRedirect = React.useCallback(() => dispatch(_auth.openAuthRedirect()), [dispatch]);
+
+  return (
+    <div className={css(styles.container)}>
+      <Header />
+
+      <div className={css(styles.content)}>
+        <div className={css(styles.signInContainerBackground)}>
+          <div className={css(styles.signInContainer)}>
+            <span className={css(styles.title)}>actually colab</span>
+            <p className={css(styles.subtitle)}>built by jeff taylor-chang and bailey tincher</p>
+
+            <div className={css(styles.signInButton)}>
+              <Button appearance="primary" onClick={dispatchOpenAuthRedirect}>
+                Sign in with your browser
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
