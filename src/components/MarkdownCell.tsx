6181bcc0c2c563b1cd9c531239417add793fd19b
Jeff Taylor-Chang <jeff@jefftc.com>
2021-02-28 15:48:41 -0500

    Added syntax highlighting to markdown code blocks

--- src/components/MarkdownCell.tsx
+++ src/components/MarkdownCell.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 import { EditorCell } from '../types/notebook';
+
+const renderers = {
+  code: ({ language, value }: { language: string; value: string }) => {
+    return <SyntaxHighlighter language={language} children={value} />;
+  },
+};
 
 /**
  * A component to render a markdown cell
@@ -10,7 +17,9 @@ import { EditorCell } from '../types/notebook';
 const MarkdownCell: React.FC<{ cell: EditorCell; onDoubleClick(): void }> = ({ cell, onDoubleClick }) => {
   return (
     <div className="markdown-container" onDoubleClick={onDoubleClick}>
-      <ReactMarkdown plugins={[gfm]}>{cell.code}</ReactMarkdown>
+      <ReactMarkdown renderers={renderers} plugins={[gfm]}>
+        {cell.code}
+      </ReactMarkdown>
     </div>
   );
 };
