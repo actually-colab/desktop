c7b8fe859f7956fb674c87f0ddbdf13e76fd7f9e
Jeff Taylor-Chang <jeff@jefftc.com>
2021-03-11 18:29:37 -0500

    Started building redux socket client middleware

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 
+import ReduxSocketClient from './middleware/ReduxSocketClient';
 import auth from './auth';
 import editor from './editor';
 import ui from './ui';
@@ -17,4 +18,4 @@ export const reducers = combineReducers({
 /**
  * The combined redux store with support for async actions
  */
-export default createStore(reducers, applyMiddleware(ReduxThunk));
+export default createStore(reducers, applyMiddleware(ReduxThunk, ReduxSocketClient()));
---
+++ src/redux/middleware/ReduxSocketClient.ts
@@ -0,0 +1,41 @@
+import { Middleware } from 'redux';
+import { ActuallyColabSocketClient } from '@actually-colab/editor-client';
+
+import { ReduxState } from '../../types/redux';
+import { AuthActionTypes, SIGN_IN } from '../../types/redux/auth';
+import { EditorActionTypes, NOTEBOOKS } from '../../types/redux/editor';
+import { httpToWebSocket } from '../../utils/request';
+
+const baseURL = httpToWebSocket(process.env.REACT_APP_AC_API_URI ?? 'http://localhost:3001/dev');
+
+/**
+ * A redux middleware for the Actually Colab socket client
+ */
+const ReduxSocketClient = (): Middleware<{}, ReduxState> => {
+  let client: ActuallyColabSocketClient | null = null;
+
+  return (store) => (next) => (action: AuthActionTypes | EditorActionTypes) => {
+    switch (action.type) {
+      case SIGN_IN.SUCCESS: {
+        client = new ActuallyColabSocketClient({
+          baseURL,
+          sessionToken: action.token,
+        });
+
+        client.on('connect', () => console.log('Connected to AC socket'));
+        client.on('close', (event) => console.log('Disconnected from AC socket', event));
+        client.on('error', (error) => console.error(error));
+        client.on('notebook_opened', (user) => console.log('Notebook opened by', user));
+        break;
+      }
+      case NOTEBOOKS.OPEN.START: {
+        console.log('Opening notebook');
+        client?.openNotebook(action.nb_id);
+      }
+    }
+
+    return next(action);
+  };
+};
+
+export default ReduxSocketClient;
