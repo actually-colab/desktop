0e4e971db327fd55e7c8aaa0da641a28eee8d405
Jeff Taylor-Chang <jeff@jefftc.com>
2021-03-19 15:26:42 -0400

    Updated husky and lint-staged to run validate only if .ts changed

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': ['yarn lint'],
+  '*.{js,jsx}': [() => 'yarn lint'],
+  '*.{ts,tsx}': [() => 'yarn lint', () => 'yarn validate'],
   '{*.json,.{babelrc,eslintrc,prettierrc}}': ['prettier --ignore-path .eslintignore --parser json --write'],
   '*.{css,scss,less}': ['prettier --ignore-path .eslintignore --single-quote --write'],
   '*.{html,md,yml}': ['prettier --ignore-path .eslintignore --single-quote --write'],
