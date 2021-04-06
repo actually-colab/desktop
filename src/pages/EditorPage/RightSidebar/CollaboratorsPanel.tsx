83e20d9d8b9db2b9e9b95596ca5c02eee197fb30
Jeff Taylor-Chang <jeff@jefftc.com>
2021-04-06 13:56:28 -0500

    Added user list to collaborator panel

--- src/pages/EditorPage/RightSidebar/CollaboratorsPanel.tsx
+++ src/pages/EditorPage/RightSidebar/CollaboratorsPanel.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { StyleSheet, css } from 'aphrodite';
 import {
   Button,
   ControlLabel,
+  FlexboxGrid,
   Form,
   FormControl,
   FormGroup,
   Icon,
+  List,
   Radio,
   RadioGroup,
   Schema,
-  SelectPicker,
 } from 'rsuite';
 import { FormInstance } from 'rsuite/lib/Form';
 import { NotebookAccessLevelType } from '@actually-colab/editor-types';
 
 import { palette, spacing } from '../../../constants/theme';
+import { ReduxState } from '../../../types/redux';
+import { UserAvatar } from '../../../components';
 
 type ShareFormValue = {
   email: string;
@@ -57,6 +60,23 @@ const styles = StyleSheet.create({
     marginLeft: spacing.DEFAULT / 4,
     fontSize: 16,
   },
+  userListContainer: {
+    marginTop: spacing.DEFAULT,
+  },
+  userList: {
+    marginTop: spacing.DEFAULT / 2,
+  },
+  userItem: {
+    backgroundColor: palette.BASE_FADED,
+  },
+  userItemLabel: {},
+  userItemEmail: {
+    color: palette.GRAY,
+  },
+  userItemName: {},
+  accessLevelIcon: {
+    color: palette.GRAY,
+  },
 });
 
 /**
@@ -65,10 +85,14 @@ const styles = StyleSheet.create({
 const CollaboratorsPanel: React.FC = () => {
   const shareFormRef = React.useRef<FormInstance>();
 
+  const notebook = useSelector((state: ReduxState) => state.editor.notebook);
+
   const [shareFormValue, setShareFormValue] = React.useState<ShareFormValue>({
     email: '',
     accessLevel: 'Read Only',
   });
+
+  const sharedUsers = React.useMemo(() => notebook?.users, [notebook?.users]);
 
   const dispatch = useDispatch();
   const dispatchShareNotebook = React.useCallback(() => shareFormRef.current?.check(), []);
@@ -100,6 +124,7 @@ const CollaboratorsPanel: React.FC = () => {
             </ControlLabel>
             <FormControl name="email" label="Email" placeholder="Email" />
           </FormGroup>
+
           <FormGroup>
             <ControlLabel>Access level</ControlLabel>
             <FormControl className={css(styles.fullPicker)} name="accessLevel" accepter={RadioGroup}>
@@ -113,6 +138,37 @@ const CollaboratorsPanel: React.FC = () => {
           <Icon icon="share" size="lg" />
           <span className={css(styles.shareText)}>Share</span>
         </Button>
+      </div>
+
+      <div className={css(styles.userListContainer)}>
+        <h6>Shared with:</h6>
+
+        <List className={css(styles.userList)}>
+          {sharedUsers?.map((item, index) => (
+            <List.Item className={css(styles.userItem)} key={item.uid} index={index}>
+              <FlexboxGrid align="middle">
+                <FlexboxGrid.Item colspan={4}>
+                  <UserAvatar user={item} hover={false} />
+                </FlexboxGrid.Item>
+
+                <FlexboxGrid.Item colspan={18}>
+                  <div className={css(styles.userItemLabel)}>
+                    <div className={css(styles.userItemEmail)}>{item.email}</div>
+                    <div className={css(styles.userItemName)}>{item.name}</div>
+                  </div>
+                </FlexboxGrid.Item>
+
+                <FlexboxGrid.Item colspan={2}>
+                  <Icon
+                    className={css(styles.accessLevelIcon)}
+                    icon={item.access_level === 'Read Only' ? 'eye' : 'pencil'}
+                    size="lg"
+                  />
+                </FlexboxGrid.Item>
+              </FlexboxGrid>
+            </List.Item>
+          ))}
+        </List>
       </div>
     </div>
   );
