f6d240c1d55af57cd3c2e8df5133be195ed8735f
Jeff Taylor-Chang <jeff@jefftc.com>
2021-04-21 18:52:10 -0500

    Updated lint-staged prettier config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   '*.{js,jsx}': [() => 'yarn lint:aggressive'],
   '*.{ts,tsx}': [() => 'yarn lint:aggressive', () => 'yarn validate'],
-  '{*.json,.{babelrc,eslintrc,prettierrc}}': ['prettier --ignore-path .eslintignore --parser json --write'],
-  '*.{css,scss,less}': ['prettier --ignore-path .eslintignore --single-quote --write'],
-  '*.{html,md,yml}': ['prettier --ignore-path .eslintignore --single-quote --write'],
+  '{*.json,.{babelrc,eslintrc,prettierrc}}': [
+    'prettier --config .prettierrc.js --ignore-path .eslintignore --parser json --write',
+  ],
+  '*.{css,scss,less}': ['prettier --config .prettierrc.js --ignore-path .eslintignore --write'],
+  '*.{html,md,yml}': ['prettier --config .prettierrc.js --ignore-path .eslintignore --write'],
 };
