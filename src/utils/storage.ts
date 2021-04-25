c5f7645c75032504a379c6dd95b34306c9e7ac94
Jeff Taylor-Chang <jeff@jefftc.com>
2021-04-25 16:05:40 -0500

    Added LocalStorageSet class

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,21 +1,97 @@
 /**
  * Make a storage object with CRUD functions
  */
-const buildLocalStorage = (key: string) => ({
+export const buildLocalStorage = (
+  key: string
+): {
+  get: () => string | null;
+  set: (value: string) => void;
+  remove: () => void;
+} => ({
   get: () => localStorage.getItem(key),
   set: (value: string) => localStorage.setItem(key, value),
   remove: () => localStorage.removeItem(key),
 });
 
-const buildWrappedLocalStorage = <T>(
+/**
+ * Make a storage object with CRUD operations plus middleware
+ */
+export const buildWrappedLocalStorage = <T>(
   key: string,
-  postProcessGet: (value: string | null) => T,
+  postProcessGet: (value: string | null) => T | null,
   preProcessSet: (value: T) => string
-) => ({
+): {
+  get: () => T | null;
+  set: (value: T) => void;
+  remove: () => void;
+} => ({
   get: () => postProcessGet(localStorage.getItem(key)),
   set: (value: T) => localStorage.setItem(key, preProcessSet(value)),
   remove: () => localStorage.removeItem(key),
 });
+
+/**
+ * A local storage implementation of a Set
+ */
+class LocalStorageSet {
+  key: string;
+  items: string[] = [];
+
+  constructor(key: string) {
+    this.key = key;
+    this.get();
+  }
+
+  /**
+   * Get the existing items in storage
+   */
+  get() {
+    const data = localStorage.getItem(this.key);
+
+    if (data) {
+      this.items = JSON.parse(data)?.items ?? [];
+    } else {
+      this.items = [];
+    }
+
+    return this.items;
+  }
+
+  /**
+   * Set the array items in storage
+   */
+  set(values: string[]) {
+    this.items = values.slice();
+
+    localStorage.setItem(
+      this.key,
+      JSON.stringify({
+        items: this.items,
+      })
+    );
+  }
+
+  /**
+   * Add an item in storage
+   */
+  add(value: string) {
+    this.set(this.items.filter((item) => item !== value).concat([value]));
+  }
+
+  /**
+   * Remove an item from storage
+   */
+  remove(value: string) {
+    this.set(this.items.filter((item) => item !== value));
+  }
+
+  /**
+   * Check if an item is in storage
+   */
+  has(value: string): boolean {
+    return this.items.includes(value);
+  }
+}
 
 /**
  * Store the session token
@@ -30,8 +106,4 @@ export const LatestNotebookIdStorage = buildLocalStorage('editor.notebook.latest
 /**
  * Store the recent contacts
  */
-export const RecentUsersStorage = buildWrappedLocalStorage<string[]>(
-  'editor.contacts.recent',
-  (value: string | null) => (value !== null ? JSON.parse(value)?.emails ?? [] : null),
-  (value) => JSON.stringify({ emails: value })
-);
+export const RecentUsersStorage = new LocalStorageSet('editor.contacts.recent');
