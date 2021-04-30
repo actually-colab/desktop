63c4c4eef3759f49e9795943f9edc56e01a94804
Jeff Taylor-Chang <jeff@jefftc.com>
2021-04-30 12:21:55 -0500

    Simplified output cell selectors

--- src/components/NotebookCell/OutputCell.tsx
+++ src/components/NotebookCell/OutputCell.tsx
@@ -23,20 +23,17 @@ const styles = StyleSheet.create({
  */
 const OutputCell: React.FC<{ cell: ImmutableEditorCell }> = ({ cell }) => {
   const selectedOutputsUid = useSelector((state: ReduxState) => state.editor.selectedOutputsUid);
-  const outputs = useSelector((state: ReduxState) => state.editor.outputs.get(cell.cell_id)?.get(selectedOutputsUid));
   const outputsMetadata = useSelector((state: ReduxState) =>
     state.editor.outputsMetadata.get(cell.cell_id)?.get(selectedOutputsUid)
   );
-
-  const runIndex = React.useMemo(() => (selectedOutputsUid === '' ? cell.runIndex : outputsMetadata?.runIndex ?? -1), [
-    cell.runIndex,
-    outputsMetadata,
-    selectedOutputsUid,
-  ]);
-  const cellOutputs = React.useMemo(() => outputs?.get(runIndex.toString()) ?? ImmutableList<ImmutableKernelOutput>(), [
-    outputs,
-    runIndex,
-  ]);
+  const cellOutputs = useSelector(
+    (state: ReduxState) =>
+      state.editor.outputs
+        .get(cell.cell_id)
+        ?.get(selectedOutputsUid)
+        ?.get((selectedOutputsUid === '' ? cell.runIndex : outputsMetadata?.runIndex ?? -1).toString()) ??
+      ImmutableList<ImmutableKernelOutput>()
+  );
 
   return (
     <div className={css(styles.container)}>
