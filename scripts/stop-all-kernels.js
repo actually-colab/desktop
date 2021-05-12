3904dbca8bc3ef6b7e09d130de26322368dcd574
Jeff Taylor-Chang <jeff@jefftc.com>
2021-05-12 08:00:16 -0500

    Kill kernel instances in parallel

--- scripts/stop-all-kernels.js
+++ scripts/stop-all-kernels.js
@@ -6,6 +6,9 @@ const promiseExec = promisify(exec);
 
 const httpSearchString = 'http://';
 
+/**
+ * Find and kill all jupyter notebook instances
+ */
 async function stopAllKernels() {
   const kernelListOutput = await promiseExec('jupyter notebook list');
 
@@ -13,26 +16,32 @@ async function stopAllKernels() {
 
   const lines = kernelListOutput.stdout.split('\n');
 
-  for (const line of lines) {
-    try {
-      const httpIndex = line.indexOf(httpSearchString);
+  await Promise.all(
+    lines.map((line) => {
+      try {
+        const httpIndex = line.indexOf(httpSearchString);
 
-      if (httpIndex >= 0) {
-        const postHttp = line.substring(httpIndex + httpSearchString.length);
-        const port = postHttp.substring(postHttp.indexOf(':') + 1, postHttp.indexOf('/?'));
+        if (httpIndex >= 0) {
+          const postHttp = line.substring(httpIndex + httpSearchString.length);
+          const port = postHttp.substring(postHttp.indexOf(':') + 1, postHttp.indexOf('/?'));
 
-        console.log(`Stopping port ${port}`);
+          console.log(`Stopping port ${port}`);
 
-        if (port.length > 0 && port.length < 6) {
-          const kernelStopOutput = await promiseExec(`jupyter notebook stop ${port}`);
-
-          console.log(kernelStopOutput.stdout || kernelStopOutput.stderr);
+          if (port.length > 0 && port.length < 6) {
+            return promiseExec(`jupyter notebook stop ${port}`)
+              .then((res) => {
+                console.log(res.stdout || res.stderr);
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }
         }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+    })
+  );
 }
 
 stopAllKernels();
