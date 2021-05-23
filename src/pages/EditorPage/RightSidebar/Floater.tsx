1aa267bbbfe4b32fc698d88c90c0d36b001b7305
Jeff Taylor-Chang <jeff@jefftc.com>
2021-05-23 16:37:13 -0400

    Minor improvements to right sidebar floater

--- src/pages/EditorPage/RightSidebar/Floater.tsx
+++ src/pages/EditorPage/RightSidebar/Floater.tsx
@@ -12,21 +12,23 @@ const styles = StyleSheet.create({
   },
   absoluteContainer: {
     position: 'absolute',
-    width: 77,
     top: spacing.DEFAULT / 2,
     right: spacing.DEFAULT / 2,
     padding: spacing.DEFAULT / 4,
     backgroundColor: palette.BASE,
     borderRadius: 6,
     border: `1px solid ${palette.BASE_BORDER}`,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
-const Floater: React.FC = () => {
+const Floater: React.FC = ({ children }) => {
   return (
     <div className={css(styles.container)}>
       <div className={css(styles.relativeContainer)}>
-        <div className={css(styles.absoluteContainer)}>Read Only</div>
+        <div className={css(styles.absoluteContainer)}>{children}</div>
       </div>
     </div>
   );
