dc390b8dad1ce134d1fc693b398c8ddc96048cf0
Jeff Taylor-Chang <jeff@jefftc.com>
2021-05-24 17:51:04 -0400

    Display a popover list of users when more than 3 users are active

--- src/pages/EditorPage/components/EditorHeader/UserAvatarList.tsx
+++ src/pages/EditorPage/components/EditorHeader/UserAvatarList.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { StyleSheet, css } from 'aphrodite';
+import { Button, Popover, Whisper } from 'rsuite';
 
-import { spacing } from '../../../../constants/theme';
 import { ReduxState } from '../../../../types/redux';
+import { spacing } from '../../../../constants/theme';
 import { UserAvatar } from '../../../../components';
 
 const styles = StyleSheet.create({
@@ -15,6 +16,29 @@ const styles = StyleSheet.create({
   avatar: {
     marginRight: spacing.DEFAULT / 2,
   },
+  popoverContainer: {
+    padding: spacing.DEFAULT,
+  },
+  userListItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userListTextContainer: {
+    marginLeft: spacing.DEFAULT,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  userTitle: {
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  userSubtitle: {
+    fontSize: 11,
+    fontWeight: 'normal',
+    display: 'flex',
+    flexDirection: 'row',
+  },
 });
 
 /**
@@ -25,11 +49,36 @@ const UserAvatarList: React.FC = () => {
 
   return (
     <div className={css(styles.avatarsContainer)}>
-      {users.map((activeUser) => (
-        <div className={css(styles.avatar)} key={activeUser.uid}>
-          <UserAvatar placement="bottomEnd" user={activeUser} />
-        </div>
-      ))}
+      {users.size <= 3 ? (
+        users.map((activeUser) => (
+          <div className={css(styles.avatar)} key={activeUser.uid}>
+            <UserAvatar user={activeUser} placement="bottomEnd" />
+          </div>
+        ))
+      ) : (
+        <Whisper
+          trigger="click"
+          placement="bottom"
+          speaker={
+            <Popover full style={{ border: '1px solid #ddd' }}>
+              <div className={css(styles.popoverContainer)}>
+                {users.map((activeUser) => (
+                  <div key={activeUser.uid} className={css(styles.userListItem)}>
+                    <UserAvatar user={activeUser} hover={false} />
+
+                    <div className={css(styles.userListTextContainer)}>
+                      <span className={css(styles.userTitle)}>{activeUser.name}</span>
+                      <span className={css(styles.userSubtitle)}>{activeUser.email}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </Popover>
+          }
+        >
+          <Button size="sm">{users.size} users</Button>
+        </Whisper>
+      )}
     </div>
   );
 };
